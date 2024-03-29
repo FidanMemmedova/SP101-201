35f263530fcdf3ad7aa079305246cd300b5d6709
Fidan Mammadova <100687487+FidanMemmedova@users.noreply.github.com>
2024-03-29 18:28:47 +0400

    Add files via upload

---
+++ JS intro/assets/js/index.js
@@ -0,0 +1,71 @@
+let numbers=[22,27,35,69,1463,15,48];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 0; i < numbers.length; i++) {
+
+//     if (numbers[i]%2==0) {
+//         console.log(numbers[i])
+//     }
+// }
+
+//27,32,40
+//2.verilmish arrayda her bir ededin 5vahid artirib yazin
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]+5);
+
+// }
+
+//3. verilmish arrayda tek ededlerin ustune 2 gelib yazdirin
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i]%2!=1){
+//         console.log(numbers[i]+2)
+//     }
+// }
+
+// let myName='Fidan'
+// myName.length=5
+
+// let students = [
+//   "Sebine",
+//   "Nurlana",
+//   "Narmin",
+//   "Leyla",
+//   "Kamil",
+//   "Diana",
+//   "Ismayil",
+//   "Inji",
+//   "Alirza",
+// ];
+// //herflerinin sayi 6 olan adlari konsolda yazdir
+
+// for (let i = 0; i < students.length; i++) {
+//   if (students[i].length==7) {
+//     console.log(students[i]);
+//   }
+// }
+
+let a=4
+let b=7
+let c=16
+// eger a+b cden kicikdirse konsolda yazsin ki kicikdirse
+// boyukdurse yazsin boyukdurse
+// beraberdirse yazsin beraberdir
